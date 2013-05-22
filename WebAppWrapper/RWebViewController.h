8db7e934880c19b539b22420bf6a9ee16f9f1842
Alex Rezit <AlexRezit@gmail.com>
2013-05-22 18:44:12 +0800

    Add RWebViewController.

---
+++ WebAppWrapper/RWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  RWebViewController.h
+//  WebAppWrapper
+//
+//  Created by Alex Rezit on 22/05/2013.
+//  Copyright (c) 2013 Seymour Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RWebViewController : UIViewController
+
+@end
---
+++ WebAppWrapper/RWebViewController.m
@@ -0,0 +1,38 @@
+//
+//  RWebViewController.m
+//  WebAppWrapper
+//
+//  Created by Alex Rezit on 22/05/2013.
+//  Copyright (c) 2013 Seymour Dev. All rights reserved.
+//
+
+#import "RWebViewController.h"
+
+@interface RWebViewController ()
+
+@end
+
+@implementation RWebViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
