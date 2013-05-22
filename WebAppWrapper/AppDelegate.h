7afbb6553857522497c4031e4eeec584fa3a479e
Alex Rezit <AlexRezit@gmail.com>
2013-05-22 18:41:38 +0800

    Initial Commit

---
+++ WebAppWrapper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WebAppWrapper
+//
+//  Created by Alex Rezit on 22/05/2013.
+//  Copyright (c) 2013 Seymour Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WebAppWrapper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WebAppWrapper
+//
+//  Created by Alex Rezit on 22/05/2013.
+//  Copyright (c) 2013 Seymour Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
